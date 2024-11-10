c20cd41a3dea3fea696e00d35251e13651246282
skabusaeed1 <shaikhabusaeed1@gmail.com>
2024-11-10 14:20:51 +0530

    create signup api

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.router.js'
 
 dotenv.config();
 
@@ -13,13 +14,16 @@ mongoose
     console.log("mongoDB is connected");
   })
   .catch((err) => {
-    console.log("err in mongoDB");
+    console.log("err is connecting to MongoDB");
   });
 
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
 
-app.use('/api',userRouter);
+app.use('/api', userRouter);
+app.use('/api/auth', authRouter);
